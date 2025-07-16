9554122ea508f3a173aca6422f5763e745a285d7
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-07-16 11:32:30 +0530

    Add routing and Bootstrap to frontend app
    Implemented React Router for navigation between Home and Shop pages. Added Bootstrap and react-bootstrap dependencies, removed default styles, and created basic Home and Shop components with Bootstrap styling.

---
+++ frontend/liyastyle-ecomm-frontend/src/components/common/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+   <>
+    <header className='shadow'>
+        <button className='btn btn-primary'>click me</button>
+    </header>
+   </>
+  )
+}
