718a79404c3564869b582b5585db792f8793ac69
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-07-24 12:40:50 +0530

    Add custom styles and update Home header UI
    Introduced a new SCSS stylesheet for custom branding and button styles, added a logo image, and refactored the Home component to use react-bootstrap Navbar with navigation links and branding. Updated main.jsx to import the new SCSS file. Updated dependencies to include sass-embedded for SCSS support.

--- frontend/liyastyle-ecomm-frontend/src/components/common/Home.jsx
+++ frontend/liyastyle-ecomm-frontend/src/components/common/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import Logo from '../../assets/image/logo-b.png';
 
 export const Home = () => {
   return (
-   <>
-    <header className='shadow'>
-        <button className='btn btn-primary'>click me</button>
-    </header>
-   </>
-  )
-}
+    <>
+      <header className="shadow">
+        <div className="bg-dark text-center">
+          <span className="text-white ">Your Fashion Partner</span>
+        </div>
+        <div className="container">
+          <Navbar expand="lg" className="">
+            <Container fluid>
+              <Navbar.Brand href="#">
+              <img src={Logo} alt="" width={100} />
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="ms-auto my-2 my-lg-0"
+                  style={{ maxHeight: "100px" }}
+                  navbarScroll
+                >
+                  <Nav.Link href="#action1">Woman</Nav.Link>
+                  <Nav.Link href="#action2">Men</Nav.Link>
+                  <Nav.Link href="#action2">Kids</Nav.Link>
+                </Nav>
+                
+               
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+      </header>
+    </>
+  );
+};
